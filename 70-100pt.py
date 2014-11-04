2422568d8f503712ccc01d78cffb8cf4dd7ea911
tmatteson1226 <s631741@student.roundrockisd.org>
2014-11-04 15:10:16 -0600

    finished 70-100 pt
    done

--- 70-100pt.py
+++ 70-100pt.py
@@ -16,8 +16,9 @@ drawpad = Canvas(root, width=800,height=600, background='white')
 player = drawpad.create_oval(390,580,410,600, fill="red")
 
 # Create your "enemies" here, before the class
-
-
+enemy1 = drawpad.create_rectangle(50,50,75,75, fill='blue')
+enemy2 = drawpad.create_rectangle(100,400,125,425, fill='blue')
+enemy3 = drawpad.create_rectangle(150,250,175,275, fill='blue')
 class MyApp:
 	def __init__(self, parent):
        	    global drawpad
@@ -27,8 +28,31 @@ class MyApp:
        	    self.up = Button(self.myContainer1)
        	    self.up.configure(text="up", background= "green")
        	    self.up.grid(row=0,column=0)
-       	    # Bind an event to the first button
        	    self.up.bind("<Button-1>", self.upClicked)
+       	    
+       	    self.myParent = parent  
+       	    self.myContainer1 = Frame(parent)
+       	    self.myContainer1.pack()
+       	    self.down = Button(self.myContainer1)
+       	    self.down.configure(text="down", background= "blue")
+       	    self.down.grid(row=0,column=1)
+       	    self.down.bind("<Button-1>", self.downClicked)
+       	    
+       	    self.myParent = parent  
+       	    self.myContainer1 = Frame(parent)
+       	    self.myContainer1.pack()
+       	    self.right = Button(self.myContainer1)
+       	    self.right.configure(text="right", background= "red")
+       	    self.right.grid(row=0,column=2)
+       	    self.right.bind("<Button-1>", self.rightClicked)
+       	    
+       	    self.myParent = parent  
+       	    self.myContainer1 = Frame(parent)
+       	    self.myContainer1.pack()
+       	    self.left = Button(self.myContainer1)
+       	    self.left.configure(text="left", background= "yellow")
+       	    self.left.grid(row=0,column=3)
+       	    self.left.bind("<Button-1>", self.leftClicked)
        	    
        	    # No need to edit this - just includes the drawpad into our frame
        	    drawpad.pack(side=RIGHT)
@@ -39,15 +63,51 @@ class MyApp:
 	    global drawpad
 	    global player
 	    # Remember to include your "enemies" with "global"
+	    global enemy1
 	    
+	    x1, y1, x2, y2 = drawpad.coords(enemy1)
+            if x2 > drawpad.winfo_width(): 
+                drawpad.move(enemy1,-800,0)
+            elif x1 < 0:
+                direction = 10
+            drawpad.move(enemy1,10,0)
+	    global enemy2
+	    x1, y1, x2, y2 = drawpad.coords(enemy2)
+            if x2 > drawpad.winfo_width(): 
+                drawpad.move(enemy2,-800,0)
+            elif x1 < 0:
+                direction = 10
+            drawpad.move(enemy2,30,0)
+	    global enemy3
+	    x1, y1, x2, y2 = drawpad.coords(enemy3)
+            if x2 > drawpad.winfo_width(): 
+                drawpad.move(enemy3,-800,0)
+            elif x1 < 0:
+                direction = 10
+            drawpad.move(enemy3,15,0)
+
 	    # Uncomment this when you're ready to test out your animation!
-	    #drawpad.after(10,self.animate)
+	    drawpad.after(10,self.animate)
 		
 	def upClicked(self, event):   
 	   global oval
 	   global player
 	   drawpad.move(player,0,-20)
-		
+	   
+        def downClicked(self, event):
+            global oval
+            global player
+            drawpad.move(player,0,20)
 
+        def rightClicked(self, event):
+            global oval
+            global player
+            drawpad.move(player,20,0)
+
+        def leftClicked(self, event):
+            global oval
+            global player
+            drawpad.move(player,-20,0)
+            
 app = MyApp(root)
 root.mainloop()
